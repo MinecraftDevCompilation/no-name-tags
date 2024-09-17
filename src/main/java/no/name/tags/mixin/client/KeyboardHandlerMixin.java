a20ddba00b4380c48b901eef379ec4ba09083c24
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2024-09-17 11:05:59 +0800

    Update KeyboardHandlerMixin.java

--- src/main/java/no/name/tags/mixin/client/KeyboardHandlerMixin.java
+++ src/main/java/no/name/tags/mixin/client/KeyboardHandlerMixin.java
@@ -32,7 +32,7 @@ public class KeyboardHandlerMixin {
 
 				if (minecraft.player != null) {
 					String state = NoNameTags.hideNameTags ? "disabled" : "enabled";
-					Component message = new Component.literal(String.format("Name tag rendering has been %s!", state));
+					Component message = Component.literal(String.format("Name tag rendering has been %s!", state));
 
 					minecraft.player.displayClientMessage(message, true);
 				}
