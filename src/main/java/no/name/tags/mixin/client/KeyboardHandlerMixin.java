21111443ce550338e037f85793a2bb3a3f011c0d
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2024-09-17 10:47:52 +0800

    Update KeyboardHandlerMixin.java

--- src/main/java/no/name/tags/mixin/client/KeyboardHandlerMixin.java
+++ src/main/java/no/name/tags/mixin/client/KeyboardHandlerMixin.java
@@ -11,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
 
 import no.name.tags.NoNameTags;
 
@@ -33,7 +32,7 @@ public class KeyboardHandlerMixin {
 
 				if (minecraft.player != null) {
 					String state = NoNameTags.hideNameTags ? "disabled" : "enabled";
-					Component message = new TextComponent(String.format("Name tag rendering has been %s!", state));
+					Component message = new Component(String.format("Name tag rendering has been %s!", state));
 
 					minecraft.player.displayClientMessage(message, true);
 				}
